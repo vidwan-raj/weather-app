a7225b04047921095ce453c8b27303be0fd0dfdf
Vidwan Raj <vidwanraj@yahoo.in>
2021-12-06 12:18:52 -0500

    Delete weather-app directory

--- weather-app/src/app/responseObj.ts
+++
@@ -1,9 +0,0 @@
-import { weatherObj } from "./weatherObj";
-export interface data {
-    main : {
-        temp: number
-    },
-    weather : any
-
-    name : String
-}
--- weather-app/src/app/weatherObj.ts
+++
@@ -1,3 +0,0 @@
-export interface weatherObj {
-    icon: String
-}
